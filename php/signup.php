3c2b68a5d5e228331890305d005bd6a61bd4981a
james shao <jshao100@gmail.com>
2016-04-24 14:18:03 -0400

    test insert

--- php/signup.php
+++ php/signup.php
@@ -14,5 +14,39 @@ if (!$conn) {
 }
 echo "Connected successfully";
 
+$name = explode(" ", $_POST['name']); //split on space
+$handle = $_POST['handle'];
+$password = $_POST['password'];
+$password2 = $_POST['password'];
+
+$error = false;
+
+//null input
+if (isset($name) || isset($handle) || $isset($password))		$error = true;
+//did not provide first and last name OR space in username
+else if (count($name) < 2 || strpos($handle, " "))				$error = true;	
+//password not the same
+else if (strcmp($password, $password2) != 0)						$error = true;	
+//password checks
+else {
+	if (strlen($password) < 8)				$error = true;
+	else if (strpos($password, " ")		$error = true;
+}
+
+if ($error) {
+	mysqli_close($conn);
+	header('Location: ../create.php');
+	exit();
+} else {
+	$query = "INSERT INTO users (user_first, user_last, user_handle, user_pass) VALUES ('"
+		. $name[0] . "','" . $name[1] . "','" . $handle . "','" . md5($password) . "')";
+
+	if (mysqli_query($conn, $query)) {
+		echo $query . " success!";
+	} else {
+		echo $query . " unsuccessful";
+	}
+}
+
 mysqli_close($conn);
 ?>
