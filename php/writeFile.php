2c72f0af60cd6441975f024874b534f1673f3455
james shao <jshao100@gmail.com>
2016-04-24 01:09:56 -0400

    create phpinfo

--- php/writeFile.php
+++ php/writeFile.php
@@ -1,19 +1,12 @@
-<html>
 <?php
-header('Location: http://maze.mybluemix.net');
-
 $arr = json_decode(str_replace('\\', '', $_POST['saveData']));
-
 $file = fopen("../mazes/test.txt","w");
-
 fwrite($file, "Hello World\n");
 foreach ($arr as $value) {
 	fwrite($file, $value);
 	fwrite($file, "\n");
 }
-
 fclose($file);
-
+header('Location: http://maze.mybluemix.net');
 exit;
 ?>
-</html>
