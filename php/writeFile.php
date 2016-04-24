e2ca7a2f58a6eafe572984d54d9d8d2458e39511
james shao <jshao100@gmail.com>
2016-04-24 00:53:59 -0400

    added new lines to save, good to go

--- php/writeFile.php
+++ php/writeFile.php
@@ -1,6 +1,6 @@
 <html>
 <?php
-header('Location: ../index.php');
+header('Location: http://maze.mybluemix.net');
 
 $arr = json_decode(str_replace('\\', '', $_POST['saveData']));
 
@@ -9,6 +9,7 @@ $file = fopen("../mazes/test.txt","w");
 fwrite($file, "Hello World\n");
 foreach ($arr as $value) {
 	fwrite($file, $value);
+	fwrite($file, "\n");
 }
 
 fclose($file);
