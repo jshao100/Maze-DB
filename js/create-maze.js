5aa809779824ef174af7114eb3ae09ce3d3c2ae7
james shao <jshao100@gmail.com>
2016-04-25 15:13:24 -0400

    css changes to create.php

--- js/create-maze.js
+++ js/create-maze.js
@@ -43,7 +43,7 @@ function createMaze(scale, w, h) {
 	
 	$(".maze").css("width",(scale*w)+2);
 	$(".maze").css("border","solid 1px black");
-	$(".save-button").children("div").children("a").css("display","block");
+	$(".save-button").children("div").children().css("display","block");
 
 	var i;
 	for(i = 0; i < h; i++) {
