7870509b070590eb02d7b2722101bd444200b55a
james shao <jshao100@gmail.com>
2016-04-25 16:15:50 -0400

    char by char

--- load.php
+++ load.php
@@ -5,7 +5,7 @@ $path = "./mazes/" . $maze_id . ".txt";
 $file = fopen($path, "r") or die("Unable to open file\n");
 
 while (!feof($file)) {
-	echo fgets($file)."<br>";
+	echo fgetc($file);
 }
 fclose($file);
 ?>
