39c84099950fcd1ca93d50c9aab2ff3ae62d25df
james shao <jshao100@gmail.com>
2016-04-25 16:54:32 -0400

    test getting data from sql for load

--- load.php
+++ load.php
@@ -1,12 +1,45 @@
 <?php include 'header.php'?>
 <?php
-$maze_id = $_GET['id'];
-$path = "./mazes/" . $maze_id . ".txt";
-$file = fopen($path, "r") or die("Unable to open file\n");
+$host = "us-cdbr-iron-east-03.cleardb.net";
+$port = "3306";
+$user = "b9a1b6108596e9";
+$pass = "cedf8312";
+$db = "ad_e15d55d16dfba74";
 
-while (!feof($file)) {
-	echo fgetc($file);
+$conn = mysqli_connect($host, $user, $pass, $db, $port);
+
+$maze_id = $_GET['id'];
+$author_id = 0;
+$author_name = "";
+$maze_date = "";
+$maze_name = "";
+$maze_data = "";
+$maze_rating = 0;
+$maze_diff = 0;
+
+$query = "SELECT * FROM mazes WHERE maze_id=" . $maze_id;
+$result = mysqli_query($conn, $query);
+while ($row = mysqli_fetch_assoc($result)) {
+	$author_id = $row['maze_author'];
+	$maze_date = $row['maze_date'];
+	$maze_name = $row['maze_name'];
+	$maze_rating = $row['maze_rating'];
+	$maze_diff = $row['maze_diff'];
+	$maze_data = $row['maze_data'];
 }
-fclose($file);
+
+$query1 = "SELECT user_handle FROM users WHERE user_id=" . $author_id;
+$result1 = mysqli_query($conn, $query1);
+while ($row = mysqli_fetch_assoc($result1)) {
+	$author_name = $row['user_handle'];
+}
+
+echo $author_name . "\n";
+echo $author_id . "\n";
+echo $maze_name . "\n";
+echo $maze_id . "\n";
+echo $maze_rating . "\n";
+echo $maze_diff . "\n";
+echo $maze_data . "\n";
 ?>
 <?php include 'footer.php'?>
